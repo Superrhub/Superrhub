632107ef116edfd54cd7dd818394e3e58ae66206
SuperT25 <superconnect@gmail.com>
2026-09-07 13:42:38 +0100

    Fix ALLOWED_HOSTS for Render

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-superhub-dev-key-change-me')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,superrhub.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
